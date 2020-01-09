c25233c4f234e9f5384608e910a6df86b405495f
Insi29 <57297663+Insi29@users.noreply.github.com>
2020-01-09 09:35:34 -0800

    Update StartActivity.java

--- StartActivity.java
+++ StartActivity.java
@@ -1,22 +1,38 @@
 package com.example.chatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class StartActivity extends AppCompatActivity {
     Button login,register;
     FirebaseUser firebaseUser;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected void onStart() {
+        super.onStart();
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
         if(firebaseUser!=null){
             Intent intent=new Intent(StartActivity.this,MainActivity.class);
             startActivity(intent);
             finish();
 
-        }login=findViewById(R.id.login);
+        }
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        login=findViewById(R.id.login);
         register=findViewById(R.id.register);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,5 +47,6 @@ public class StartActivity extends AppCompatActivity {
                 startActivity(new Intent(StartActivity.this,RegisterActivity.class));
             }
         });
+    }
 }
-}
+
