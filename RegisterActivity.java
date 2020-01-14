7a64663341f5d57617c2a42aa3a48fa965bed567
Insi29 <57297663+Insi29@users.noreply.github.com>
2020-01-14 07:02:54 -0800

    Update RegisterActivity.java

--- RegisterActivity.java
+++ RegisterActivity.java
@@ -1,4 +1,5 @@
 package com.example.chatapp;
+package com.example.talksquad;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -7,11 +8,10 @@ import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
-
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -21,6 +21,7 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.rengwuxian.materialedittext.MaterialEditText;
+
 import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
@@ -75,7 +76,7 @@ public class RegisterActivity extends AppCompatActivity {
                     assert firebaseUser != null;
                     String userid = firebaseUser.getUid();
                     reference= FirebaseDatabase.getInstance().getReference("Users").child(userid);
-                    HashMap <String,String> hashMap =new HashMap<>();
+                    HashMap<String,String> hashMap =new HashMap<>();
                     hashMap.put("Id",userid);
                     hashMap.put("username",username);
                     hashMap.put("ImageURL","default");
